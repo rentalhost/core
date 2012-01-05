d4c3a096c435d7bde26af1388c1b96edaba2dda0
David Rodrigues <david.proweb@gmail.com>
2012-01-05 18:02:35 -0200

    - core_view: aceitar dados protegidos de variáveis externas em path.

--- modules/core/core_constants.php
+++ modules/core/core_constants.php
@@ -10,12 +10,13 @@
 
 	// Define o tipo de informação aceito em um nome de arquivo ou pasta
 	// Organizado por ordem de prioridade; apenas caracteres singulares para flexibilidade de sub-definições
-	define( 'CORE_VALID_PATH_ID', '[a-z_0-9A-Z\x7f-\xff]*' );
+	define( 'CORE_VALID_PATH_ID', '[a-z_0-9A-Z\x7f-\xff]' );
 	// Define o ID de variáveis (usado em nomes de classes ou métodos, por exemplo)
-	define( 'CORE_VALID_ID', '[a-z_A-Z\x7f-\xff][a-z_0-9A-Z\x7f-\xff]*' );
+	define( 'CORE_VALID_ID', '[a-z_A-Z\x7f-\xff]' . CORE_VALID_PATH_ID . '*' );
 
 	// Define um path válido para views e outras ocasiões
-	define( 'CORE_VALID_PATH', '~^(' . CORE_VALID_PATH_ID . '/?)+$~' );
+	// http://stackoverflow.com/a/8748376/755393
+	define( 'CORE_VALID_PATH', '~^(?:(?:' . CORE_VALID_PATH_ID . '*|\[' . CORE_VALID_PATH_ID . '+\])(?=/|$)/?)+$~' );
 
 	// Define o separador de chamada (usado para separar classe de método, como a::b)
 	define( 'CORE_VALID_CALLER', '~^(?:(?<object>' . CORE_VALID_ID . ')::)?(?<method>' . CORE_VALID_ID . ')$~' );
--- modules/test/classes/core_view.php
+++ modules/test/classes/core_view.php
@@ -3,10 +3,10 @@
 	// classe core_view
 	class unit_core__view_library extends test_class_library {
 		public function test_load() {
-			$this->test(1, load(''));
-			$this->test(2, load('../try/to/hack'), 'Trying to hack views, it\'s impossible!');
-			$this->test(3, load('/test/useful/fake'), 'File not exists');
-			$this->test(4, load('/test/useful/sub_test'), 'File not exists');
+			$this->test(1, load('', null, true));
+			$this->test(2, load('../try/to/hack', null, true), 'Trying to hack views, it\'s impossible!');
+			$this->test(3, load('/test/useful/fake', null, true), 'File not exists');
+			$this->test(4, load('/test/useful/sub_test', null, true), 'File not exists');
 
 			$this->set_prefix('read');
 			$view = load('/test/useful/test', null, true);
@@ -34,5 +34,15 @@
 			$this->test(2, $view->has_failed());
 			$this->test(3, $view->get_contents());
 			$this->test(4, $view->get_return());
+
+			$this->set_prefix('secure_test');
+			$insegure_flag = core_view::STATUS_VIEW_IS_INSECURE;
+			$this->test(2, (load('abc', null, true)->get_status() & $insegure_flag) !== $insegure_flag);
+			$this->test(3, (load('abc/def/ghi', null, true)->get_status() & $insegure_flag) !== $insegure_flag);
+			$this->test(4, (load('abc/[def]/ghi', null, true)->get_status() & $insegure_flag) !== $insegure_flag);
+			$this->test(5, (load('abc/[def/ghi]', null, true)->get_status() & $insegure_flag) !== $insegure_flag);
+			$this->test(6, (load('[abc]', null, true)->get_status() & $insegure_flag) !== $insegure_flag);
+			$this->test(7, (load('[]', null, true)->get_status() & $insegure_flag) !== $insegure_flag);
+			$this->test(8, (load('[/]', null, true)->get_status() & $insegure_flag) !== $insegure_flag);
 		}
 	}
