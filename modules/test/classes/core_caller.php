21e96fd42e23193db14d442804518338d5dbf523
David Rodrigues <david.proweb@gmail.com>
2012-01-18 08:57:57 -0200

    - tests, core_caller: melhora no teste;

--- modules/test/classes/core_caller.php
+++ modules/test/classes/core_caller.php
@@ -4,8 +4,9 @@
 	class unit_core__caller_library extends test_class_library {
 		public function test_call() {
 			$this->set_prefix( 'caller' );
-			$this->test( 1, call(), 'Instance create' );
-			$this->test( 2, call(), 'Instance retrieval' );
+			$caller = call();
+			$this->test( 1, $caller, 'Instance create' );
+			$this->test( 2, call() === $caller, 'Instance retrieval' );
 
 			$this->set_prefix('exception');
 			$this->exception_test(1, 'exception_invalid_caller');
