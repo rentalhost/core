51ab590ef6ffbbe0522f51dce2d909bad4a8fc94
David Rodrigues <david.proweb@gmail.com>
2012-02-10 01:07:57 -0200

    - core: carregar as configurações somente após o autoloader;

--- modules/core/core_engine.php
+++ modules/core/core_engine.php
@@ -10,11 +10,13 @@
 
 	// Inclui alguns arquivos importantes
 	require_once 'core_constants.php';
-	require_once 'core_configs.php';
 	require_once 'core_helpers.php';
 
 	// Ao chamar uma classe, o core ficará responsável por identificar sua localização
 	spl_autoload_register( 'core::__autoload' );
+
+	// Carrega as configurações
+	require_once 'core_configs.php';
 
 	// Executa o controlador na URL
 	core_controller::_init_url();
